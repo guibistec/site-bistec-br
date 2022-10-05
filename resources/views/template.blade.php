9ac02a78954622f356daffb679dd00ae0a0671e8
guibistec <guilhermebistec@gmail.com>
2022-10-05 11:13:39 -0300

    teste css

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -19,7 +19,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Bistec Consultoria de TI. Desde 1999 fornecendo produtos e serviços de Automação Comercial e Informática. Aproveite o melhor da tecnologia para seu negócio.">
-    <title>Início - Bistec Brasil</title>
+    <title>Iníciooi - Bistec Brasil</title>
     @yield('style')
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/styleMobile.css">
