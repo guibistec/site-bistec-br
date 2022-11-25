3737a406e4afb487e8f1e53a37498fc211787a5d
guicrispa <guilhermebistec@gmail.com>
2022-11-25 17:50:17 -0300

    página suporte responsiva mobile e pc

--- resources/views/suporte.blade.php
+++ resources/views/suporte.blade.php
@@ -14,18 +14,14 @@
             <p>Caso a sua empresa ainda não seja parceira, fale conosco!
             </p>
             <p>Faça Download do Anydesk!</p>
-            <div class="bt-dialog-anydesk">
-                <a href="https://download.anydesk.com/AnyDesk.exe" class="button_text">DOWNLOAD ANYDESK</a>                   
-            </div>
-
+                <a href="https://download.anydesk.com/AnyDesk.exe" class="bt-dialog-anydesk">DOWNLOAD ANYDESK</a>                   
+            
             <p>Faça Download do TeamViewer!</p>
-            <div class="bt-dialog-teamviewer">
-                <a href="http://download.teamviewer.com/download/TeamViewerQS_pt.exe" class="button_text">DOWNLOAD TEAMVIEWER</a>                   
-            </div>
+                <a class="bt-dialog-teamviewer" href="http://download.teamviewer.com/download/TeamViewerQS_pt.exe">DOWNLOAD TEAMVIEWER</a>                   
 
             <!-- <p>Fale conosco também pelo <strong><a class="suporte-wpp" href="https://api.whatsapp.com/send?phone=5513992101945" target="_blank">WhatsApp Business no +55 13 4042-0777</a></strong> ou pelo <strong><a href="https://t.me/BISTEC_BOT" target="_blank" class="suporte-telegram">Telegram – Procure por Atendimento BISTEC.</a></strong> </p> -->
 
-            <p>
+            
                 <div class="suporte_icons">
                    <a class="icon_wpp_support" href="https://api.whatsapp.com/send?phone=5513992101945" target="_blank"><ion-icon name="logo-whatsapp"></ion-icon></a>
                     <h5>Suporte Técnico pelo Whatsapp</h5>
@@ -35,7 +31,7 @@
                    <a class="icon_telegram_support"href="https://t.me/BISTEC_BOT" target="_blank"><ion-icon name="paper-plane-outline"></ion-icon></a>
                     <h5>Suporte Técnico pelo Telegram</h5>
                 </div>
-            </p>
+            
         
         </section>
     </article>
